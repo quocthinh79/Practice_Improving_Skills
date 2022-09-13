f90ee4dc970dd0a9d46e1f8600aee69775e4c4e2
unlocker1205 <19130215@st.hcmuaf.edu.vn>
2022-09-13 11:34:07 +0700

    Hoàn thành bài redux cơ bản

--- src/pages/ReduxBasic/index.js
+++ src/pages/ReduxBasic/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
+import { addNewHobby, setActionHobby } from "~/actions/hobby";
+import HobbyList from "./HobbyList";
 
 const ReduxBasicPage = () => {
-    return (
-        <div>
-            Redux Page
-        </div>
-    );
+  const hobbyList = useSelector((state) => state.hobby.list);
+  const activeId = useSelector((state) => state.hobby.activeId);
+  const dispatch = useDispatch();
+
+  const randomNumber = () => Math.trunc(Math.random() * 1000);
+  const handleRandomHobbyClick = () => {
+    const randomId = randomNumber();
+    const newHobby = {
+      id: randomId,
+      title: `Fake Hobby ${randomId}`,
+    };
+
+    const action = addNewHobby(newHobby);
+    dispatch(action);
+  };
+
+  const handleHobbyItemClick = (hobby) => {
+    const action = setActionHobby(hobby);
+    dispatch(action);
+  };
+
+  return (
+    <>
+      <div>Redux Page</div>
+      <button onClick={handleRandomHobbyClick}>Random hobby click</button>
+      <HobbyList
+        hobbyList={hobbyList}
+        activeId={activeId}
+        onHobbyClick={handleHobbyItemClick}
+      ></HobbyList>
+    </>
+  );
 };
 
 export default ReduxBasicPage;
