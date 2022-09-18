e65a6d7bbc2111e66fe1172290bfcf6b9c05304a
unlocker1205 <19130215@st.hcmuaf.edu.vn>
2022-09-18 20:47:48 +0700

    Chỉnh sửa feed data weather trang thoitiet.vn

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,14 @@ import ReduxBasicPage from "../pages/ReduxBasic";
 import HomePage from "../pages/Home";
 import WeartherDataFeedPage from "~/pages/WeatherDataFeed";
 import Lab1Animation from "~/pages/Lab1Animation";
+import WeatherDataFeed2 from "~/pages/WeatherDataFeed2";
 
 const publicRoutes = [
   { path: routesConfig[0].path, page: HomePage },
   { path: routesConfig[1].path, page: ReduxBasicPage },
   { path: routesConfig[2].path, page: WeartherDataFeedPage },
-  { path: routesConfig[3].path, page: Lab1Animation },
+  { path: routesConfig[3].path, page: WeatherDataFeed2 },
+  { path: routesConfig[4].path, page: Lab1Animation },
 ];
 
 const privateRoutes = [];
